a40576fc45c7551769ecfe61ac006bc6f2bdd592
vishnuu123pl <137207623+vishnuu123pl@users.noreply.github.com>
2024-06-18 20:40:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '22182189'))
 API_HASH = environ.get('API_HASH', '5e7c4088f8e23d0ab61e29ae11960bf5')
-BOT_TOKEN = environ.get('BOT_TOKEN', "6621594376:AAGCwfRJKYFVyyACBA5PFxDpPzzye95MmwU")
+BOT_TOKEN = environ.get('BOT_TOKEN', "6375200576:AAGVJdULNUtf1S2qRaZrXvXlVVytXuzJ-OQ")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
